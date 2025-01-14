c6876653cc5faf58affb713908ad68247fb9fb81
Adam Mocarski <admocarski@gmail.com>
2025-01-14 16:01:38 +0100

    added main menu navigation

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import ApolloProvider from "@/providers/apollo-provider";
+import MainMenu from "@/components/main-menu";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -21,6 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.className} antialiased`}>
+        <MainMenu />
         <ApolloProvider>{children}</ApolloProvider>
       </body>
     </html>
