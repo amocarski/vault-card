e2a28fc80fa87a0bd17135099bea1cee97c5e789
Adam Mocarski <admocarski@gmail.com>
2025-01-14 16:00:13 +0100

    improved DataCard component to make sure gap between targetRef element matches

--- components/ui/data-card.tsx
+++ components/ui/data-card.tsx
@@ -4,40 +4,63 @@ import * as React from "react";
 import * as Portal from "@radix-ui/react-portal";
 
 import { Card, CardContent } from "@/components/ui/card";
+import { useEffect, useRef } from "react";
+import { cn } from "@/lib/utils";
 
 type DataCardProps = {
   children: React.ReactNode;
   open: boolean;
   targetRef: React.RefObject<HTMLElement>;
-};
+} & React.HTMLAttributes<HTMLDivElement>;
 
-const DataCard = ({ open, targetRef, children }: DataCardProps) => {
-  const [position, setPosition] = React.useState({ top: 0, left: 0, width: 0 });
+const DataCard = ({ open, targetRef, children, className }: DataCardProps) => {
+  const [position, setPosition] = React.useState({
+    top: 0,
+    left: 0,
+    width: 0,
+  });
+
+  const observerRef = useRef<MutationObserver | null>(null);
 
   const updatePosition = React.useCallback(() => {
     if (targetRef.current) {
       const rect = targetRef.current.getBoundingClientRect();
       setPosition({
-        top: rect.bottom + window.scrollY + 4, // 4px gap
+        top: rect.bottom + window.scrollY + 10,
         left: rect.left + window.scrollX,
         width: rect.width,
       });
     }
   }, [targetRef]);
 
-  React.useEffect(() => {
+  useEffect(() => {
     updatePosition();
     window.addEventListener("resize", updatePosition);
-    return () => window.removeEventListener("resize", updatePosition);
-  }, [updatePosition]);
+
+    if (targetRef.current) {
+      observerRef.current = new MutationObserver(updatePosition);
+      observerRef.current.observe(targetRef.current, {
+        attributes: true,
+        childList: true,
+        subtree: true,
+      });
+    }
+
+    return () => {
+      window.removeEventListener("resize", updatePosition);
+      if (observerRef.current) {
+        observerRef.current.disconnect();
+      }
+    };
+  }, [updatePosition, targetRef]);
 
   if (!open) return null;
 
   return (
-    <Portal.Root>
+    <Portal.Root className={cn(className)}>
       <Card
         data-dropdown
-        className="absolute z-50 h-fit max-h-[310px] overflow-y-auto !rounded-corner-radius border-[0.5px] animate-in fade-in-0 zoom-in-95"
+        className="absolute z-50 h-fit max-h-[310px] w-full overflow-y-auto !rounded-corner-radius border-[0.5px] animate-in fade-in-0 zoom-in-95"
         style={{
           top: position.top,
           left: position.left,
